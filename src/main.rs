04ec64f23ebdd059cbfac76d2c845c268058ca14
Frosty <frosty@archtide.space>
2025-06-21 13:36:20 -0400

    Planets or something

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,63 @@
-use macroquad::prelude::*;
+use std::f32::consts::TAU;
+
+use macroquad::{prelude::*, time};
+
+struct Planet {
+    pub radius: f32,
+    pub color: Color,
+    pub orbital_radius: f32,
+    pub orbital_period: f32,
+}
+
+impl Default for Planet {
+    fn default() -> Self {
+        Self {
+            radius: 1.,
+            color: WHITE,
+            orbital_radius: 0.,
+            orbital_period: 1.,
+        }
+    }
+}
+
+impl Planet {
+    fn new(radius: f32, color: Color, orbital_radius: f32, orbital_period: Option<f32>) -> Self {
+        Self {
+            radius,
+            color,
+            orbital_period: orbital_period.unwrap_or(orbital_radius * orbital_radius),
+            orbital_radius,
+        }
+    }
+}
 
 #[macroquad::main("macroquad_test")]
 async fn main() {
-    let fragment_shader = FRAGMENT_SHADER.to_string();
-    let vertex_shader = VERTEX_SHADER.to_string();
-
-    let pipeline_params = PipelineParams {
-        depth_write: true,
-        depth_test: Comparison::LessOrEqual,
-        ..Default::default()
-    };
-
-    let material = load_material(
-        ShaderSource::Glsl {
-            vertex: &vertex_shader,
-            fragment: &fragment_shader,
-        },
-        MaterialParams {
-            pipeline_params,
-            ..Default::default()
-        },
-    )
-    .unwrap();
-
-    let camera = Camera2D::default();
+    let planets = vec![
+        Planet::new(3., YELLOW, 0., Some(1.)),
+        Planet::new(0.5, RED, 4., None),
+        Planet::new(1., GREEN, 10., None),
+    ];
 
     loop {
-        clear_background(WHITE);
+        clear_background(BLACK);
+
+        let aspect_ratio = screen_width() as f32 / screen_height() as f32;
+        let mut camera =
+            Camera2D::from_display_rect(Rect::new(-aspect_ratio, -1., aspect_ratio * 2., 2.));
+
+        camera.zoom = vec2(0.1 / aspect_ratio, 0.1);
 
         set_camera(&camera);
 
-        gl_use_material(&material);
-        draw_rectangle(-1., -1., 2., 2., WHITE);
-        gl_use_default_material();
+        for planet in planets.iter() {
+            let angle = -(time::get_time() as f32 / planet.orbital_period).fract() * TAU;
+            let position = vec2(angle.cos(), angle.sin()) * planet.orbital_radius;
+            draw_poly(position.x, position.y, 64, planet.radius, 0., planet.color);
+        }
 
         set_default_camera();
 
         next_frame().await;
     }
 }
-
-const FRAGMENT_SHADER: &'static str = "#version 100
-precision lowp float;
-
-varying vec2 uv;
-
-void main() {
-    gl_FragColor = vec4(uv, 0.0, 1.0);
-}
-";
-
-const VERTEX_SHADER: &'static str = "#version 100
-precision lowp float;
-
-attribute vec3 position;
-attribute vec2 texcoord;
-
-varying vec2 uv;
-
-uniform mat4 Model;
-uniform mat4 Projection;
-
-void main() {
-    gl_Position = Projection * Model * vec4(position, 1);
-    uv = texcoord;
-}
-";
